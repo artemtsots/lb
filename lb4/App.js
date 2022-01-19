47d4899241758355bdd6bd43c993d949f19de1d2
artemtsots <60147228+artemtsots@users.noreply.github.com>
2022-01-19 23:27:15 +0200

    Add files via upload

---
+++ lb4/App.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import './styles/App.css'
+
+function App() {
+  return (
+    <div className="App">
+     <div className="content">
+       
+      <b>Вебтехнологоії та вебдизайн</b>
+     <div>
+       Під час цього курсу я вивчу основи розробки веб додатків на реакті
+     </div>
+
+
+     </div>
+     <div className="button">
+      <button>Clear</button>
+      <p/> <p/>
+      <b>Уильям Буршье</b>
+     <div>
+     Ultraviolence (с англ. — «Ультранасилие») — 
+     третий студийный альбом американской певицы и автора песен Ланы Дель Рей
+     </div>
+
+
+     </div>
+     <div className="button">
+      <button>Clear</button>
+
+      <p/> <p/>
+      <b>Изображение дня</b>
+     <div>
+     изданный 13 июня 2014 года на лейблах Interscope и Polydor.
+    После выхода в 2012 году Born to Die Лана не планировала выпускать новую
+     </div>
+
+
+     </div>
+     <div className="button">
+      <button>Clear</button>
+
+      <p/> <p/>
+      <b>Текущие события</b>
+     <div>
+     Однако к ноябрю 2013 года она собрала материал и приступила к записи новой пластинки
+     </div>
+
+
+     </div>
+     <div className="button">
+      <button>Clear</button>
+
+      <p/> <p/>
+      <b>«Неделе Штата Вашингтон»</b>
+     <div>
+     В декабре работа подошла к концу, после чего певица познакомилась
+      с Дэном Ауэрбахом из The Black Keys
+     </div>
+
+
+     </div>
+     <div className="button">
+      <button>Clear</button>
+     </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
